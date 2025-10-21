ec18fd9ecffda7416dda67d62c85bf0ca9cadd7b
Rafiq <muhraf.livify@gmail.com>
2025-10-21 22:30:48 +0400

    feat: Add required open-next.config.ts file for adapter

---
+++ open-next.config.ts
@@ -0,0 +1,7 @@
+import { OpenNextConfig } from "@opennextjs/cloudflare";
+
+const config: OpenNextConfig = {
+  // Configuration can go here if needed, but for now, an empty object is fine.
+};
+
+export default config;
