ffa14905d2f658b14c23f2648b7b8d937af96c69
Muhraf2020 <143078152+Muhraf2020@users.noreply.github.com>
2025-10-24 09:43:46 +0400

    Update open-next.config.ts

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,27 +1,3 @@
-// open-next.config.ts
 import { defineCloudflareConfig } from "@opennextjs/cloudflare";
 
-export default defineCloudflareConfig({
-  default: {
-    override: {
-      wrapper: "cloudflare-node",
-      converter: "edge",
-      proxyExternalRequest: "fetch",
-      incrementalCache: "dummy",
-      tagCache: "dummy",
-      queue: "dummy",
-    },
-  },
-  edgeExternals: ["node:crypto"],
-  middleware: {
-    external: true,
-    override: {
-      wrapper: "cloudflare-edge",
-      converter: "edge",
-      proxyExternalRequest: "fetch",
-      incrementalCache: "dummy",
-      tagCache: "dummy",
-      queue: "dummy",
-    },
-  },
-});
+export default defineCloudflareConfig();
